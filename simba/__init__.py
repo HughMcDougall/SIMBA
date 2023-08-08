10268d29431c3aa86a7a5c27076d30809958f495
HughMcDougall <hughmcdougallemail@gmail.com>
2023-08-08 22:31:33 +1000

    atgkajgkajg

--- simba/__init__.py
+++ simba/__init__.py
@@ -1,1 +1,8 @@
-from SIMBA import *
+__author__ = "Hugh McDougall"
+__email__ = "hughmcdougallemaiL@gmail.com"
+__uri__ = "https://github.com/HughMcDougall/SIMBA/tree/main/simba"
+__license__ = "Open"
+__description__ = "A package test"
+__copyright__ = "N/A"
+__contributors__ = "HM, TD, BP"
+__bibtex__ = __citation__ = """TBD"""
